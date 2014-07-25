2ad7e98ab91cb1207e786f8016e6849c620c6903
valmon2 <valmon2@radiant.dreamhost.com>
2014-07-25 08:35:07 -0700

    arreglado settings

--- entrechat/urls.py
+++ entrechat/urls.py
@@ -12,6 +12,8 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
 	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # Uncomment the next line to enable the admin:
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+                           {'document_root': settings.STATIC_ROOT}),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$','principal.views.inicio'),
     url(r'^indumentarias/$','principal.views.Indumentarias'),
